e2d083099f353bd8842c9475dc3efff132b2e82a
László Kiss Kollár <lkisskollar@bloomberg.net>
2021-01-08 15:28:42 +0000

    Move native extension test into separate file
    The `test_smoke.py` has been used to collect all sorts of unrelated
    tests. This moves the test for multithreaded extensions into a separate
    file.

--- tests/integration/test_smoke.py
+++ tests/integration/test_smoke.py
@@ -1,16 +1,10 @@
 import mmap
-import shutil
-import subprocess
-import sys
 import threading
 from pathlib import Path
 
 from bloomberg.pensieve import AllocatorType
 from bloomberg.pensieve import Tracker
 from bloomberg.pensieve import start_thread_trace
-
-HERE = Path(__file__).parent
-TEST_MULTITHREADED_EXTENSION = HERE / "multithreaded_extension"
 
 
 def allocating_function():
@@ -84,44 +78,3 @@ def test_smoke_in_a_thread(tmpdir):
     assert "allocating_function" in {
         element[0] for element in mmunmap_record.stack_trace()
     }
-
-
-def test_multithreaded_extension(tmpdir, monkeypatch):
-    """Test tracking allocations in a native extension which spawns multiple threads,
-    each thread allocating and freeing memory."""
-    # GIVEN
-    output = Path(tmpdir) / "test.bin"
-    extension_name = "multithreaded_extension"
-    extension_path = tmpdir / extension_name
-    shutil.copytree(TEST_MULTITHREADED_EXTENSION, extension_path)
-    subprocess.run(
-        [sys.executable, str(extension_path / "setup.py"), "build_ext", "--inplace"],
-        check=True,
-        cwd=extension_path,
-        capture_output=True,
-    )
-
-    # WHEN
-    with monkeypatch.context() as ctx:
-        ctx.setattr(sys, "path", [*sys.path, str(extension_path)])
-        from testext import run
-
-        with Tracker(output) as tracker:
-            run()
-
-    # THEN
-    records = list(tracker.get_allocation_records())
-    assert records
-
-    memaligns = [
-        record for record in records if record.allocator == AllocatorType.MEMALIGN
-    ]
-    assert len(memaligns) == 100 * 100  # 100 threads allocate 100 times in testext
-    memaligns_addr = {record.address for record in memaligns}
-    memalign_frees = [
-        record
-        for record in records
-        if record.address in memaligns_addr and record.allocator == AllocatorType.FREE
-    ]
-
-    assert len(memalign_frees) >= 100 * 100
