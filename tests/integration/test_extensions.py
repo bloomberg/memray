b1adbf7c056518125187d25b0f21ba55b65ea9ba
László Kiss Kollár <lkisskollar@bloomberg.net>
2021-01-12 11:07:54 +0000

    test: Add tests for misbehaving extensions
    This adds tests which check that tracking extensions which acquire the
    GIL on a thread and do not register trace functions work correctly.

--- tests/integration/test_extensions.py
+++ tests/integration/test_extensions.py
@@ -5,9 +5,11 @@ from pathlib import Path
 
 from bloomberg.pensieve import AllocatorType
 from bloomberg.pensieve import Tracker
+from bloomberg.pensieve._test import MemoryAllocator
 
 HERE = Path(__file__).parent
 TEST_MULTITHREADED_EXTENSION = HERE / "multithreaded_extension"
+TEST_MISBEHAVING_EXTENSION = HERE / "misbehaving_extension"
 
 
 def test_multithreaded_extension(tmpdir, monkeypatch):
@@ -41,6 +43,10 @@ def test_multithreaded_extension(tmpdir, monkeypatch):
         record for record in records if record.allocator == AllocatorType.MEMALIGN
     ]
     assert len(memaligns) == 100 * 100  # 100 threads allocate 100 times in testext
+
+    # We don't keep track of the native stacks. Make sure they are empty
+    assert all(len(memalign.stack_trace()) == 0 for memalign in memaligns)
+
     memaligns_addr = {record.address for record in memaligns}
     memalign_frees = [
         record
@@ -49,3 +55,58 @@ def test_multithreaded_extension(tmpdir, monkeypatch):
     ]
 
     assert len(memalign_frees) >= 100 * 100
+
+
+def test_misbehaving_extension(tmpdir, monkeypatch):
+    """Check that we can correctly track allocations in an extension which invokes
+    Python code in a thread and does not register trace functions."""
+    # GIVEN
+    output = Path(tmpdir) / "test.bin"
+    extension_name = "misbehaving_extension"
+    extension_path = tmpdir / extension_name
+    shutil.copytree(TEST_MISBEHAVING_EXTENSION, extension_path)
+    subprocess.run(
+        [sys.executable, str(extension_path / "setup.py"), "build_ext", "--inplace"],
+        check=True,
+        cwd=extension_path,
+        capture_output=True,
+    )
+
+    def allocating_function():
+        allocator = MemoryAllocator()
+        allocator.valloc(1234)
+        allocator.free()
+
+    # WHEN
+    with monkeypatch.context() as ctx:
+        ctx.setattr(sys, "path", [*sys.path, str(extension_path)])
+        from misbehaving import call_fn
+
+        with Tracker(output) as tracker:
+            call_fn(allocating_function)
+
+    # THEN
+    allocations = list(tracker.get_allocation_records())
+    allocs = [
+        event
+        for event in allocations
+        if event.size == 1234 and event.allocator == AllocatorType.VALLOC
+    ]
+    assert len(allocs) == 1
+    (alloc,) = allocs
+
+    stack_trace = alloc.stack_trace()
+    assert len(stack_trace)
+
+    *_, bottom_frame = stack_trace
+    func, filename, line = bottom_frame
+    assert func == "allocating_function"
+    assert filename.endswith(__file__)
+    assert line == 77
+
+    frees = [
+        event
+        for event in allocations
+        if event.address == alloc.address and event.allocator == AllocatorType.FREE
+    ]
+    assert len(frees) >= 1
