924152ccb1ba2cb06bbd83716b9b13fd867f7e0b
Pradyun Gedam <pgedam@bloomberg.net>
2021-01-26 11:15:24 +0000

    Fail gracefully when no command is given to run
    This avoids an IndexError-with-a-traceback crash on invalid user input,
    by requiring a single "module" argument and allowing additional optional
    arguments.
    
    Authored-by: Pradyun Gedam <pgedam@bloomberg.net>
    Reviewed-by: László Kiss Kollár <lkisskollar@bloomberg.net>
    Signed-off-by: Pradyun Gedam <pgedam@bloomberg.net>

--- src/bloomberg/pensieve/commands/run.py
+++ src/bloomberg/pensieve/commands/run.py
@@ -15,17 +15,22 @@ class RunCommand:
             "--output",
             help="Output file name (default: <process_name>.<pid>.out)",
         )
-        parser.add_argument("command", help="Command to run", nargs=argparse.REMAINDER)
+        parser.add_argument("module", help="Module to run")
+        parser.add_argument(
+            "module_args",
+            help="Additional arguments for module",
+            nargs=argparse.REMAINDER,
+        )
 
     def main(self, args: argparse.Namespace) -> int:
         results_file = (
             args.output
             if args.output is not None
-            else f"{args.command[0]}.{os.getpid()}.out"
+            else f"{args.module}.{os.getpid()}.out"
         )
         print(f"Writing profile results into {results_file}")
         with Tracker(results_file):
-            sys.argv = args.command
-            runpy.run_module(args.command[0], run_name="__main__")
+            sys.argv[1:] = args.module_args
+            runpy.run_module(args.module, run_name="__main__", alter_sys=True)
 
         return 0
--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -18,13 +18,24 @@ def test_no_args_passed(capsys):
 @patch("bloomberg.pensieve.commands.run.runpy")
 @patch("bloomberg.pensieve.commands.run.os.getpid")
 class TestRunSubCommand:
+    def test_run_without_arguments(self, getpid_mock, runpy_mock, tracker_mock, capsys):
+        with pytest.raises(SystemExit):
+            main(["run"])
+
+        captured = capsys.readouterr()
+        assert "error: the following arguments are required: module" in captured.err
+
     def test_run_default_output(self, getpid_mock, runpy_mock, tracker_mock):
         getpid_mock.return_value = 0
         assert 0 == main(["run", "foobar"])
-        runpy_mock.run_module.assert_called_with("foobar", run_name="__main__")
+        runpy_mock.run_module.assert_called_with(
+            "foobar", run_name="__main__", alter_sys=True
+        )
         tracker_mock.assert_called_with("foobar.0.out")
 
     def test_run_override_output(self, getpid_mock, runpy_mock, tracker_mock):
         assert 0 == main(["run", "--output", "my_output", "foobar"])
-        runpy_mock.run_module.assert_called_with("foobar", run_name="__main__")
+        runpy_mock.run_module.assert_called_with(
+            "foobar", run_name="__main__", alter_sys=True
+        )
         tracker_mock.assert_called_with("my_output")
