e938de06c081521520225471d1ada34d87249a98
Pablo Galindo Salgado <pablogsal@gmail.com>
2021-12-07 13:13:16 +0000

    Don't print error message if the reader reached EOF
    We are incorrectly printing an error message for a failed close if the
    `FileSource` reaches EOF and any of the read operations failed because
    of it. This makes us incorrectly identify the error bits in the file
    stream as coming from the `close()` call. To avoid this, clean the error
    status in the file stream before attempting to close.
    
    Authored-by: Pablo Galindo <pgalindo3@bloomberg.net>
    Reviewed-by: László Kiss Kollár <lkisskollar@bloomberg.net>
    Reviewed-by: Pradyun Gedam <pgedam@bloomberg.net>
    Signed-off-by: Pablo Galindo <pgalindo3@bloomberg.net>

--- src/bloomberg/pensieve/_pensieve/source.cpp
+++ src/bloomberg/pensieve/_pensieve/source.cpp
@@ -55,7 +55,7 @@ void
 FileSource::_close()
 {
     d_stream.close();
-    if (d_stream.fail()) {
+    if (d_stream.fail() && !d_stream.eof()) {
         // d_file_name might have been already destroyed at this point, so don't
         // try to print it
         std::cerr << "Failed to close output file, results might be incomplete" << std::endl;
