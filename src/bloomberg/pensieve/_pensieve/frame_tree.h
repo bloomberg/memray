f4a2170f30b76d67e0e20696ee861e79887fb1ff
Matt Wozniski <mwozniski@bloomberg.net>
2022-04-01 18:07:08 -0400

    Restructure `FrameTree` to benefit `RecordReader`
    By exposing a new method from `FrameTree` for resolving a child stack
    index given a parent stack index and a child frame id, we can
    drastically speed up the record reader. Previously, each time an
    allocation record was received, the record reader would need to start
    a search from the root of the frame tree down to the frame where the
    allocation occurred. Now, the reader can resolve the stack ids as frames
    are pushed and popped, and it only ever needs to descend one level from
    the last stack ID it knew, instead of needing to start the search over
    from the tree root.
    
    Some back of the napkin profiling suggests that this makes the record
    reader about 50% faster, as this tree walking was where the record
    reader would spend the majority of its time.

--- src/bloomberg/pensieve/_pensieve/frame_tree.h
+++ src/bloomberg/pensieve/_pensieve/frame_tree.h
@@ -10,65 +10,67 @@ class FrameTree
 {
   public:
     using index_t = uint32_t;
-    struct FrameNode
-    {
-        frame_id_t frame_id;
-        index_t parent_index;
-    };
 
-    inline const FrameNode& nextNode(index_t index) const
+    inline std::pair<frame_id_t, index_t> nextNode(index_t index) const
     {
         std::lock_guard<std::mutex> lock(d_mutex);
         assert(1 <= index && index <= d_graph.size());
-        return d_graph[index - 1];
+        return std::make_pair(d_graph[index].frame_id, d_graph[index].parent_index);
     }
 
     using tracecallback_t = std::function<bool(frame_id_t, index_t)>;
-
-    template<typename T>
-    size_t getTraceIndex(const T& stack_trace)
-    {
-        return getTraceIndex(stack_trace, [](frame_id_t, index_t) { return true; });
-    }
 
     template<typename T>
     size_t getTraceIndex(const T& stack_trace, const tracecallback_t& callback)
     {
         std::lock_guard<std::mutex> lock(d_mutex);
         index_t index = 0;
-        FrameEdge* parent = &d_root;
         for (const auto& frame : stack_trace) {
-            auto it = std::lower_bound(
-                    parent->children.begin(),
-                    parent->children.end(),
-                    frame,
-                    [](const FrameEdge& edge, const tracking_api::frame_id_t frame_id) {
-                        return edge.frame_id < frame_id;
-                    });
-            if (it == parent->children.end() || it->frame_id != frame) {
-                index_t new_index = d_current_tree_index++;
-                it = parent->children.insert(it, {frame, new_index, {}});
-                if (!callback(frame, parent->index)) {
-                    return 0;
-                }
-                d_graph.push_back({frame, parent->index});
-            }
-            index = it->index;
-            parent = &(*it);
+            index = getTraceIndexUnsafe(index, frame, callback);
         }
         return index;
     }
 
+    size_t getTraceIndex(index_t parent_index, frame_id_t frame)
+    {
+        std::lock_guard<std::mutex> lock(d_mutex);
+        return getTraceIndexUnsafe(parent_index, frame, tracecallback_t());
+    }
+
   private:
-    struct FrameEdge
+    size_t getTraceIndexUnsafe(index_t parent_index, frame_id_t frame, const tracecallback_t& callback)
+    {
+        Node& parent = d_graph[parent_index];
+        auto it = std::lower_bound(parent.children.begin(), parent.children.end(), frame);
+        if (it == parent.children.end() || it->frame_id != frame) {
+            index_t new_index = d_graph.size();
+            it = parent.children.insert(it, {frame, new_index});
+            if (callback && !callback(frame, parent_index)) {
+                return 0;
+            }
+            d_graph.push_back({frame, parent_index});
+        }
+        return it->child_index;
+    }
+
+    struct DescendentEdge
     {
         frame_id_t frame_id;
-        index_t index;
-        std::vector<FrameEdge> children;
+        index_t child_index;
+
+        bool operator<(frame_id_t frame_id)
+        {
+            return this->frame_id < frame_id;
+        }
     };
-    FrameEdge d_root = {0, 0, {}};
-    size_t d_current_tree_index = 1;
+
+    struct Node
+    {
+        frame_id_t frame_id;
+        index_t parent_index;
+        std::vector<DescendentEdge> children;
+    };
     mutable std::mutex d_mutex;
-    std::vector<FrameNode> d_graph{};
+    std::vector<Node> d_graph{{0, 0, {}}};
 };
 }  // namespace pensieve::tracking_api
