bloomberg/memray

commit d168b0ac6ccb853f7483de77ba250145c329a6cf

Store count of frames to pop in FRAME_POP flags
This both simplifies the tracking API, pushing some encoding complexity
down into the record writer, and also saves space in the output file.

Signed-off-by: Matt Wozniski <mwozniski@bloomberg.net>

src/memray/_memray/record_writer.h
@@ -110,11 +110,19 @@ bool inline RecordWriter::writeThreadSpecificRecord(thread_id_t tid, const T& it
 
 bool inline RecordWriter::writeRecordUnsafe(const FramePop& record)
 {
-    static_assert(std::is_trivially_copyable<FramePop>::value, "FramePop cannot be trivially copied");
+    size_t count = record.count;
+    while (count) {
+        uint8_t to_pop = (count > 64 ? 64 : count);
+        count -= to_pop;
 
-    RecordTypeAndFlags token{RecordType::FRAME_POP, 0};
-    return d_sink->writeAll(reinterpret_cast<const char*>(&token), sizeof(token))
-           && d_sink->writeAll(reinterpret_cast<const char*>(&record), sizeof(record));
+        to_pop -= 1;  // i.e. 0 means pop 1 frame, 63 means pop 64 frames
+        RecordTypeAndFlags token{RecordType::FRAME_POP, to_pop};
+        if (!d_sink->writeAll(reinterpret_cast<const char*>(&token), sizeof(token))) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 bool inline RecordWriter::writeRecordUnsafe(const FramePush& record)

src/memray/_memray/record_reader.cpp
@@ -161,9 +161,10 @@ RecordReader::processFramePush(const FramePush& record)
 }
 
 bool
-RecordReader::parseFramePop(FramePop* record)
+RecordReader::parseFramePop(FramePop* record, unsigned int flags)
 {
-    return d_input->read(reinterpret_cast<char*>(record), sizeof(*record));
+    record->count = flags + 1;
+    return true;
 }
 
 bool
@@ -441,7 +442,7 @@ RecordReader::nextRecord()
             } break;
             case RecordType::FRAME_POP: {
                 FramePop record;
-                if (!parseFramePop(&record) || !processFramePop(record)) {
+                if (!parseFramePop(&record, record_type_and_flags.flags) || !processFramePop(record)) {
                     if (d_input->is_open()) LOG(ERROR) << "Failed to process frame pop";
                     return RecordResult::ERROR;
                 }
@@ -705,11 +706,11 @@ RecordReader::dumpAllRecords()
                 printf("FRAME_POP ");
 
                 FramePop record;
-                if (!parseFramePop(&record)) {
+                if (!parseFramePop(&record, record_type_and_flags.flags)) {
                     Py_RETURN_NONE;
                 }
 
-                printf("count=%u\n", record.count);
+                printf("count=%zd\n", record.count);
             } break;
             case RecordType::FRAME_INDEX: {
                 printf("FRAME_ID ");

src/memray/_memray/record_reader.h
@@ -83,7 +83,7 @@ class RecordReader
     [[nodiscard]] bool parseFramePush(FramePush* record);
     [[nodiscard]] bool processFramePush(const FramePush& record);
 
-    [[nodiscard]] bool parseFramePop(FramePop* record);
+    [[nodiscard]] bool parseFramePop(FramePop* record, unsigned int flags);
     [[nodiscard]] bool processFramePop(const FramePop& record);
 
     [[nodiscard]] bool parseFrameIndex(tracking_api::pyframe_map_val_t* pyframe_val);

src/memray/_memray/records.h
@@ -197,7 +197,7 @@ struct FramePush
 
 struct FramePop
 {
-    uint8_t count;
+    size_t count;
 };
 
 struct UnresolvedNativeFrame

src/memray/_memray/tracking_api.cpp
@@ -612,16 +612,11 @@ Tracker::registerFrame(const RawFrame& frame)
 bool
 Tracker::popFrames(uint32_t count)
 {
-    while (count) {
-        uint8_t to_pop = (count > 255 ? 255 : count);
-        count -= to_pop;
-
-        const FramePop entry{to_pop};
-        if (!d_writer->writeThreadSpecificRecord(thread_id(), entry)) {
-            std::cerr << "memray: Failed to write output, deactivating tracking" << std::endl;
-            deactivate();
-            return false;
-        }
+    const FramePop entry{count};
+    if (!d_writer->writeThreadSpecificRecord(thread_id(), entry)) {
+        std::cerr << "memray: Failed to write output, deactivating tracking" << std::endl;
+        deactivate();
+        return false;
     }
     return true;
 }

commit 0e3b72a0a9496068513eb9e77a7a884bd5036c4d

Fix popping more than 16 frames with `popFrames`
Fix a bug introduced in:

    d168b0a Store count of frames to pop in FRAME_POP flags

which fortunately never made it into a released version. That commit
changed our tracker to pack the number of frames being popped into
a bit field, and handle the case where the number of frames to pop was
too large for the bit field by looping and emitting multiple records.

But, the check to see whether the number of frames to pop was too large
for the bit field was incorrect - the bit field is only 4 bits wide, and
so can only hold a maximum value of 15, not 63.

Signed-off-by: Matt Wozniski <mwozniski@bloomberg.net>

src/memray/_memray/record_writer.h
@@ -137,10 +137,10 @@ bool inline RecordWriter::writeRecordUnsafe(const FramePop& record)
 {
     size_t count = record.count;
     while (count) {
-        uint8_t to_pop = (count > 64 ? 64 : count);
+        uint8_t to_pop = (count > 16 ? 16 : count);
         count -= to_pop;
 
-        to_pop -= 1;  // i.e. 0 means pop 1 frame, 63 means pop 64 frames
+        to_pop -= 1;  // i.e. 0 means pop 1 frame, 15 means pop 16 frames
         RecordTypeAndFlags token{RecordType::FRAME_POP, to_pop};
         assert(token.flags == to_pop);
         if (!writeSimpleType(token)) {