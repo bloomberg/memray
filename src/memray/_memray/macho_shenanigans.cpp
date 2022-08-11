ddd72391f6b3e92d3fd286c8e250f72e4fe94351
Pablo Galindo <pablogsal@gmail.com>
2022-08-11 18:04:39 +0100

    Add linker patching backend for macOS
    To allow to patch symbols in macOS binaries, add a symbol patching
    backend implementation based on the dyld linker.
    
    Signed-off-by: Pablo Galindo <pablogsal@gmail.com>

---
+++ src/memray/_memray/macho_shenanigans.cpp
@@ -0,0 +1,152 @@
+#include <cstring>
+#include <dlfcn.h>
+
+#include "hooks.h"
+#include "linker_shenanigans.h"
+#include "logging.h"
+#include "macho_utils.h"
+
+#ifndef __APPLE__
+#    error "This file can only be compiled in MacOS systems"
+#endif
+
+namespace memray::linker {
+
+template<typename Hook>
+static void
+patch_symbol(
+        const Hook& hook,
+        typename Hook::signature_t intercept,
+        const char* symname,
+        void* addr,
+        vm_size_t section_size,
+        bool restore_original)
+{
+    kern_return_t err = vm_protect(
+            mach_task_self(),
+            reinterpret_cast<uintptr_t>(addr),
+            section_size,
+            0,
+            VM_PROT_READ | VM_PROT_WRITE | VM_PROT_COPY);
+    if (err == KERN_SUCCESS) {
+        auto typedAddr = reinterpret_cast<typename Hook::signature_t*>(addr);
+        *typedAddr = restore_original ? hook.d_original : intercept;
+        LOG_IF_DEBUG(symname << " intercepted!");
+    } else {
+        LOG(ERROR) << "Failed to patch " << symname;
+    }
+}
+
+static void
+patch_symbols_in_section(
+        const section_t* section,
+        uintptr_t slide,
+        const DynamicInfoTable& dyninfo_table,
+        bool restore_original)
+{
+    auto symbol_addr_table = reinterpret_cast<void**>(slide + section->addr);
+    for (unsigned int i = 0; i < section->size / sizeof(void*); i++) {
+        const char* symbol_name = dyninfo_table.getSymbol(section->reserved1, i);
+        if (!symbol_name || !(symbol_name[0] == '_' || symbol_name[0] == '.') || !symbol_name[1]) {
+            continue;
+        }
+#define FOR_EACH_HOOKED_FUNCTION(hookname)                                                              \
+    if (strcmp(hooks::hookname.d_symbol, symbol_name + 1) == 0) {                                       \
+        patch_symbol(                                                                                   \
+                hooks::hookname,                                                                        \
+                &intercept::hookname,                                                                   \
+                symbol_name,                                                                            \
+                symbol_addr_table + i,                                                                  \
+                section->size,                                                                          \
+                restore_original);                                                                      \
+        continue;                                                                                       \
+    }
+        MEMRAY_HOOKED_FUNCTIONS
+#undef FOR_EACH_HOOKED_FUNCTION
+    }
+}
+
+static void
+patch_symbols_in_shared_object(
+        const struct mach_header* header,
+        intptr_t slide,
+        const char* image_name,
+        bool restore_original,
+        std::set<std::string>& patched)
+{
+    if (!restore_original) {
+        if (patched.find(image_name) != patched.end()) {
+            // We already patched this library
+            return;
+        }
+        patched.insert(image_name);
+    }
+
+    if (strstr(image_name, "memray.cpython") || strstr(image_name, "/dyld")
+        || strstr(image_name, "dyld.dylib"))
+    {
+        LOG_IF_DEBUG("Skipping patching symbols for " << image_name);
+        return;
+    }
+    LOG_IF_DEBUG("Patching symbols for " << image_name);
+
+    auto segment_filter = [](const char* seg_name) {
+        return strcmp(seg_name, SEG_DATA) == 0 || strcmp(seg_name, SEG_DATA_CONST) == 0
+               || strcmp(seg_name, SEG_AUTH_CONST) == 0;
+    };
+
+    DynamicInfoTable dyninfo_table(header, slide, segment_filter);
+    if (!dyninfo_table) {
+        LOG_IF_DEBUG("Could not construct dynamic information table" << image_name);
+        return;
+    }
+
+    LOG_IF_DEBUG("Found " << dyninfo_table.segments.size() << " data segments");
+
+    for (const auto& segment_cmd : dyninfo_table.segments) {
+        const auto current_seg = reinterpret_cast<uintptr_t>(segment_cmd);
+        auto section_head = reinterpret_cast<const section_t*>(current_seg + sizeof(segment_command_t));
+        LOG_IF_DEBUG("Considering segment " << segment_cmd->segname);
+        for (size_t i = 0; i < segment_cmd->nsects; i++) {
+            const section_t* section = section_head + i;
+            unsigned int section_type = section->flags & SECTION_TYPE;
+            if (section_type != S_LAZY_SYMBOL_POINTERS && section_type != S_NON_LAZY_SYMBOL_POINTERS) {
+                LOG_IF_DEBUG("Skipping section" << i << " (" << section->segname << ")");
+                continue;
+            }
+            LOG_IF_DEBUG("Patching section number " << i << " (" << section->segname << ")");
+            patch_symbols_in_section(section, slide, dyninfo_table, restore_original);
+        }
+    }
+}
+
+static void
+patch_symbols_in_all_shared_objects(bool restore_original, std::set<std::string>& patched)
+{
+    if (restore_original) {
+        patched.clear();
+    }
+    uint32_t c = _dyld_image_count();
+    for (uint32_t i = 0; i < c; i++) {
+        const struct mach_header* header = _dyld_get_image_header(i);
+        intptr_t slide = _dyld_get_image_vmaddr_slide(i);
+        const char* image_name = _dyld_get_image_name(i);
+        patch_symbols_in_shared_object(header, slide, image_name, restore_original, patched);
+    }
+}
+
+/* Public API functions */
+
+void
+SymbolPatcher::overwrite_symbols() noexcept
+{
+    patch_symbols_in_all_shared_objects(false, symbols);
+}
+
+void
+SymbolPatcher::restore_symbols() noexcept
+{
+    patch_symbols_in_all_shared_objects(true, symbols);
+}
+
+}  // namespace memray::linker
---
+++ src/memray/_memray/macho_utils.h
@@ -0,0 +1,109 @@
+#pragma once
+
+#include <cstring>
+#include <dlfcn.h>
+#include <mach-o/dyld.h>
+#include <mach-o/loader.h>
+#include <mach-o/nlist.h>
+#include <mach/mach.h>
+#include <mach/vm_map.h>
+#include <mach/vm_region.h>
+#include <sys/types.h>
+#include <vector>
+
+#include "hooks.h"
+#include "linker_shenanigans.h"
+#include "logging.h"
+
+#ifndef __APPLE__
+#    error "This file can only be compiled in MacOS systems"
+#endif
+
+#ifdef __LP64__
+typedef struct mach_header_64 mach_header_t;
+typedef struct segment_command_64 segment_command_t;
+typedef struct section_64 section_t;
+typedef struct nlist_64 nlist_t;
+#    define ARCH_LC_SEGMENT LC_SEGMENT_64
+#else
+typedef struct mach_header mach_header_t;
+typedef struct segment_command segment_command_t;
+typedef struct section section_t;
+typedef struct nlist nlist_t;
+#    define ARCH_LC_SEGMENT LC_SEGMENT
+#endif
+
+#ifndef SEG_DATA_CONST
+#    define SEG_DATA_CONST "__DATA_CONST"
+#endif
+
+#ifndef SEG_AUTH_CONST
+#    define SEG_AUTH_CONST "__AUTH_CONST"
+#endif
+
+struct DynamicInfoTable
+{
+    using segment_filter_t = std::function<bool(const char* segname)>;
+
+    const nlist_t* symbol_table = nullptr;
+    const char* string_table = nullptr;
+    const uint32_t* dynsym_table = nullptr;
+    std::vector<const segment_command_t*> segments;
+
+    explicit DynamicInfoTable(
+            const struct mach_header* header,
+            uintptr_t slide,
+            const segment_filter_t& segment_filter)
+    {
+        const segment_command_t* linkedit_cmd = nullptr;
+        const struct symtab_command* symtab_cmd = nullptr;
+        const struct dysymtab_command* dysym_cmd = nullptr;
+
+        const segment_command_t* current_segment_cmd = nullptr;
+        uintptr_t current_cmd = reinterpret_cast<uintptr_t>(header) + sizeof(mach_header_t);
+        for (uint i = 0; i < header->ncmds; i++, current_cmd += current_segment_cmd->cmdsize) {
+            current_segment_cmd = reinterpret_cast<const segment_command_t*>(current_cmd);
+            switch (current_segment_cmd->cmd) {
+                case ARCH_LC_SEGMENT: {
+                    const char* segname = current_segment_cmd->segname;
+                    if (strcmp(segname, SEG_LINKEDIT) == 0) {
+                        linkedit_cmd = current_segment_cmd;
+                    }
+                    if (segment_filter(segname)) {
+                        segments.emplace_back(current_segment_cmd);
+                    }
+                } break;
+                case LC_SYMTAB:
+                    symtab_cmd = reinterpret_cast<const symtab_command*>(current_segment_cmd);
+                    break;
+                case LC_DYSYMTAB:
+                    dysym_cmd = reinterpret_cast<const dysymtab_command*>(current_segment_cmd);
+                    break;
+            }
+        }
+
+        if (!linkedit_cmd || !symtab_cmd || !dysym_cmd) {
+            return;
+        }
+        const auto linkedit_base = slide + linkedit_cmd->vmaddr - linkedit_cmd->fileoff;
+        symbol_table = reinterpret_cast<nlist_t*>(linkedit_base + symtab_cmd->symoff);
+        string_table = reinterpret_cast<char*>(linkedit_base + symtab_cmd->stroff);
+        dynsym_table = reinterpret_cast<uint32_t*>(linkedit_base + dysym_cmd->indirectsymoff);
+    }
+
+    explicit operator bool() const
+    {
+        return symbol_table && string_table && dynsym_table;
+    }
+
+    const char* getSymbol(uintptr_t section_offset, unsigned long index) const
+    {
+        auto index_into_symtable = (dynsym_table + section_offset)[index];
+        if (index_into_symtable & (INDIRECT_SYMBOL_ABS | INDIRECT_SYMBOL_LOCAL)) {
+            return nullptr;
+        }
+
+        uint32_t string_table_offset = symbol_table[index_into_symtable].n_un.n_strx;
+        return string_table + string_table_offset;
+    }
+};
