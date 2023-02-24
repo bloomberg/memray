53f27d1274ed81c90c239a4d1d8712b20e080e8c
Matt Wozniski <mwozniski@bloomberg.net>
2023-02-23 19:20:09 -0500

    Handle CLI arguments in `HighWatermarkCommand`
    All of the commands that make use of the `HighWatermarkCommand` base
    class accept a very similar set of command line arguments, with only
    minor variations. The only differences are:
    
    - Only `memray flamegraph` supports `--temporal-leaks`
    - Only `memray flamegraph` supports `--split-threads`
    - Only `memray transform` has a required positional argument
    
    Set up the `argparse` parser in the base class. Make it choose whether
    or not to add the `--temporal-leaks` argument based on whether or not
    a `temporal_reporter_factory` was provided. Extend the base class
    implementation for `memray flamegraph` and `memray transform` to add
    support for the other reporter-specific arguments.
    
    Signed-off-by: Matt Wozniski <mwozniski@bloomberg.net>

--- src/memray/commands/common.py
+++ src/memray/commands/common.py
@@ -3,6 +3,7 @@ import os
 import pathlib
 import sys
 from pathlib import Path
+from textwrap import dedent
 from typing import Any
 from typing import Iterable
 from typing import Optional
@@ -176,6 +177,65 @@ class HighWatermarkCommand:
                 show_memory_leaks=show_memory_leaks,
                 merge_threads=merge_threads,
             )
+
+    def prepare_parser(self, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument(
+            "-o",
+            "--output",
+            help="Output file name",
+            default=None,
+        )
+        parser.add_argument(
+            "-f",
+            "--force",
+            help="If the output file already exists, overwrite it",
+            action="store_true",
+            default=False,
+        )
+        alloc_type_group = parser.add_mutually_exclusive_group()
+        alloc_type_group.add_argument(
+            "--leaks",
+            help="Show memory leaks, instead of peak memory usage",
+            action="store_true",
+            dest="show_memory_leaks",
+            default=False,
+        )
+        if self.temporal_reporter_factory:
+            alloc_type_group.add_argument(
+                "--temporal-leaks",
+                help=(
+                    "Generate a dynamic flame graph showing allocations performed"
+                    " in a user-selected time range and not freed before the end"
+                    " of that time range."
+                ),
+                action="store_true",
+                default=False,
+            )
+        alloc_type_group.add_argument(
+            "--temporary-allocation-threshold",
+            metavar="N",
+            help=dedent(
+                """
+                Report temporary allocations, as opposed to leaked allocations
+                or high watermark allocations.  An allocation is considered
+                temporary if at most N other allocations occur before it is
+                deallocated.  With N=0, an allocation is temporary only if it
+                is immediately deallocated before any other allocation occurs.
+                """
+            ),
+            action="store",
+            dest="temporary_allocation_threshold",
+            type=int,
+            default=-1,
+        )
+        alloc_type_group.add_argument(
+            "--temporary-allocations",
+            help="Equivalent to --temporary-allocation-threshold=1",
+            action="store_const",
+            dest="temporary_allocation_threshold",
+            const=1,
+        )
+        parser.add_argument("results", help="Results of the tracker run")
 
     def run(
         self, args: argparse.Namespace, parser: argparse.ArgumentParser, **kwargs: Any
