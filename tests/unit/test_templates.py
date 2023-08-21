68a453a25528403180508a6fd48dd1f800dcc8a8
Ivona Stojanovic <istojanovic2@bloomberg.net>
2023-08-21 11:36:06 +0100

    tests: Inverted flame graph
    Added tests for inverted flame graph.
    
    Signed-off-by: Ivona Stojanovic <istojanovic2@bloomberg.net>

--- tests/unit/test_templates.py
+++ tests/unit/test_templates.py
@@ -4,13 +4,20 @@ from memray.reporters.templates import get_report_title
 
 
 @pytest.mark.parametrize(
-    ["kind", "show_memory_leaks", "expected"],
+    ["kind", "show_memory_leaks", "inverted", "expected"],
     [
-        ("flamegraph", False, "flamegraph report"),
-        ("flamegraph", True, "flamegraph report (memory leaks)"),
-        ("table", False, "table report"),
-        ("table", True, "table report (memory leaks)"),
+        ("flamegraph", False, False, "flamegraph report"),
+        ("flamegraph", True, False, "flamegraph report (memory leaks)"),
+        ("table", False, False, "table report"),
+        ("table", True, False, "table report (memory leaks)"),
+        ("flamegraph", False, True, "inverted flamegraph report"),
+        ("flamegraph", True, True, "inverted flamegraph report (memory leaks)"),
     ],
 )
-def test_title_for_regular_report(kind, show_memory_leaks, expected):
-    assert get_report_title(kind=kind, show_memory_leaks=show_memory_leaks) == expected
+def test_title_for_regular_report(kind, show_memory_leaks, inverted, expected):
+    assert (
+        get_report_title(
+            kind=kind, show_memory_leaks=show_memory_leaks, inverted=inverted
+        )
+        == expected
+    )
