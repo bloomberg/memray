56d5b3ab48d24edc10b18221e52d75ba4d981d19
Matt Wozniski <mwozniski@bloomberg.net>
2023-10-03 20:13:38 -0400

    Drop `--heap-limit` from `memray attach`
    This is not working as expected on i686 for reasons we haven't yet
    tracked down, though it works on aarch64 and x86-64. Given that the name
    is misleading (it's actually looking at maximum RSS size, not at heap
    size) and the implementation is less robust than we'd like, we're going
    to drop this feature in order to unblock a release containing the other
    `memray attach` improvements.
    
    We expect to add a feature to the `Tracker` itself in the future to
    support automatically deactivating itself when a certain heap size is
    reached.
    
    Signed-off-by: Matt Wozniski <mwozniski@bloomberg.net>

--- tests/unit/test_attach.py
+++ tests/unit/test_attach.py
@@ -20,26 +20,3 @@ class TestAttachSubCommand:
         captured = capsys.readouterr()
         print("Error", captured.err)
         assert "Can't use aggregated mode without an output file." in captured.err
-
-
-class TestAttachSubCommandOptions:
-    @pytest.mark.parametrize(
-        "arg1,arg2",
-        [
-            ("--heap-limit=10", "--duration=10"),
-        ],
-    )
-    def test_memray_attach_stop_tracking_option_with_other_mode_options(
-        self, arg1, arg2, capsys
-    ):
-        # WHEN
-        with pytest.raises(SystemExit):
-            main(["attach", "1234", arg1, arg2])
-
-        captured = capsys.readouterr()
-        arg1_name = arg1.split("=")[0]
-        arg2_name = arg2.split("=")[0]
-        assert (
-            f"argument {arg2_name}: not allowed with argument {arg1_name}"
-            in captured.err
-        )
