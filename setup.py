c8c4ccf302d3f5a584377fecc57baeb09d3e99c6
Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>
2024-09-08 13:35:40 +0300

    Add Python 3.7 Trove classifier
    Signed-off-by: Hugo van Kemenade <1324225+hugovk@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -314,6 +314,7 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: POSIX :: Linux",
         "Operating System :: MacOS",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
