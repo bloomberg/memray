b5c09f7a3da1bf18a8f5063a016cef623fc794e4
Kira Kaviani <chriswhoprograms@gmail.com>
2025-05-22 10:37:44 -0400

    Refactor fix
    Signed-off-by: Kira Kaviani <chriswhoprograms@gmail.com>

--- setup.py
+++ setup.py
@@ -333,33 +333,21 @@ setup(
         compiler_directives=COMPILER_DIRECTIVES,
     ),
     include_package_data=False,
-    #Wildcards are necessary to capture some extensions (*.html, *.gdb, *.lldb)
     package_data={
-        "memray": [
+        "": [
             "*.py",
             "*.pyi",
-            "*.typed",
             "*.cpp",
-        ],
-        "_ipython": [
-            "*.py"
-        ],
-        "memray.commands": [
-            "*", 
-        ],
-        "memray.reporters": [
-            "*.py",
-            "*.css",
             "*.js",
+            "*.css",
+            "*.html",
+            "*.gdb",
+            "*.lldb",
+            "*.typed"
         ],
         "memray.reporters.templates": [
             "assets/*.css",
             "assets/*.js",
-            "*", 
-        ],
-        "memray.reporters.assets": [
-            "*.css",
-            "*.js",
         ]
     },
     install_requires=install_requires,
