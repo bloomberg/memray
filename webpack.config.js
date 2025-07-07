24239719172b42e719c71ce26375cf958350b1c7
Pablo Galindo <pablogsal@gmail.com>
2025-07-07 01:55:17 +0100

    Add build system for vendor asset bundling
    This commit introduces the infrastructure needed to bundle external CDN
    dependencies as local assets for offline use. The package.json now
    includes all necessary vendor dependencies including Bootstrap, jQuery,
    D3, DataTables, and Plotly.js. The webpack configuration has been
    updated with CopyWebpackPlugin to automatically copy minified assets
    from node_modules to a local vendor directory during the build process.
    
    The Makefile has been enhanced with new targets: build-vendor creates
    the vendor assets, the main build target now includes vendor asset
    generation, and the clean target removes the vendor directory. This
    build system enables the upcoming --no-web flag functionality that
    allows memray to generate HTML reports that work without internet
    access.
    
    Signed-off-by: Pablo Galindo <pablogsal@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   mode: "production",
@@ -24,4 +25,58 @@ module.exports = {
       }),
     ],
   },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "node_modules/bootstrap/dist/css/bootstrap.min.css",
+          to: "vendor/bootstrap.min.css",
+        },
+        {
+          from: "node_modules/jquery/dist/jquery.min.js",
+          to: "vendor/jquery.min.js",
+        },
+        {
+          from: "node_modules/popper.js/dist/umd/popper.min.js",
+          to: "vendor/popper.min.js",
+        },
+        {
+          from: "node_modules/bootstrap/dist/js/bootstrap.min.js",
+          to: "vendor/bootstrap.min.js",
+        },
+        {
+          from: "node_modules/lodash/lodash.min.js",
+          to: "vendor/lodash.min.js",
+        },
+        {
+          from: "node_modules/plotly.js-dist-min/plotly.min.js",
+          to: "vendor/plotly.min.js",
+        },
+        {
+          from: "node_modules/d3/build/d3.min.js",
+          to: "vendor/d3.v4.min.js",
+        },
+        {
+          from: "node_modules/d3-scale-chromatic/dist/d3-scale-chromatic.min.js",
+          to: "vendor/d3-scale-chromatic.v1.min.js",
+        },
+        {
+          from: "node_modules/d3-tip/dist/index.js",
+          to: "vendor/d3-tip.min.js",
+        },
+        {
+          from: "node_modules/d3-flame-graph/dist/d3-flamegraph.min.js",
+          to: "vendor/d3-flamegraph.min.js",
+        },
+        {
+          from: "node_modules/datatables.net/js/jquery.dataTables.min.js",
+          to: "vendor/jquery.dataTables.min.js",
+        },
+        {
+          from: "node_modules/datatables.net-bs4/js/dataTables.bootstrap4.min.js",
+          to: "vendor/dataTables.bootstrap4.min.js",
+        },
+      ],
+    }),
+  ],
 };
