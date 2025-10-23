a4196cd59512f2d5ffa158d7a04ee61b4d2c88cc
Jesse Carnaxide <jcarnaxide@gmail.com>
2025-10-23 07:49:59 -0400

    Tutorial exercise 1 uses example that is more conducive to learning
    Signed-off-by: Jesse Carnaxide <jcarnaxide@gmail.com>

--- docs/tutorials/tests/test_exercise_1.py
+++ docs/tutorials/tests/test_exercise_1.py
@@ -6,9 +6,9 @@ from exercise_1.fibonacci import generate_fibonacci_hash
 
 @pytest.mark.limit_memory("100 KB")
 def test_fibonacci():
-    LENGTH_OF_SEQUENCE_1 = 33333  # pylint: disable=invalid-name
-    LENGTH_OF_SEQUENCE_2 = 30000  # pylint: disable=invalid-name
-    LENGTH_OF_SEQUENCE_3 = 34567  # pylint: disable=invalid-name
+    LENGTH_OF_SEQUENCE_1 = 11111  # pylint: disable=invalid-name
+    LENGTH_OF_SEQUENCE_2 = 22222  # pylint: disable=invalid-name
+    LENGTH_OF_SEQUENCE_3 = 33333  # pylint: disable=invalid-name
 
     generate_fibonacci_hash(
         LENGTH_OF_SEQUENCE_1, LENGTH_OF_SEQUENCE_2, LENGTH_OF_SEQUENCE_3
